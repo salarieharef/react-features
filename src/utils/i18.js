1cb1701cef6b5db716f54be2fd1578c6b93ba0ae
aref <aref@gmail.com>
2025-04-12 12:10:40 +0330

    debug

--- src/utils/i18.js
+++ src/utils/i18.js
@@ -2,13 +2,9 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import locale from './locale'
 
-
-// npm install react-i18next i18next --save
-
-
 const resources = {
   en: {
-    translatio : locale.en
+    translation: locale.en
   },
   fa: {
     translation: locale.fa
@@ -26,4 +22,4 @@ i18n
     }
   });
 
-  export default i18n;
+export default i18n;
